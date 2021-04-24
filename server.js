0b480aa68be7a820630f4d108c788362df354661
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-04-24 14:20:46 +0530

    db connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
@@ -9,6 +10,18 @@ app.set('view engine', 'ejs');
 app.listen(port,()=>{
     console.log("Your App is running on the Port " + port);
 })
+
+mongoose.connect('mongodb://localhost/tanmay_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(console.log("DB connected successfully")).catch(
+    (err)=>{
+        console.log(err);
+        process.abort();
+    }
+)
 
 app.get("/",(req,res)=>{
     res.send(["Hello world","Welcome","Express JS"]);
